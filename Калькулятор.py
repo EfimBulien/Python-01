7cfecfab7fa39d18af7d65f6ec8952c4ea75dee3
Ефим Бульен <143902970+EfimBulien@users.noreply.github.com>
2023-09-27 17:20:43 +0300

    Update Калькулятор.py

--- Калькулятор.py
+++ Калькулятор.py
@@ -1,50 +1,94 @@
 import math
 while True:
-    operation = int(input('Выберите математическую операцию: \nСложение -- 1\nВычитание -- 2\nУмножение -- 3\nДеление -- 4\nВозведение в степень -- 5\nВычисление квадратного корня -- 6\nВычисление синуса -- 7\nВычисление косинуса -- 8\nВычисление тангенса -- 9\nВычисление факториала -- 10\n'))
+    operation = int(input('\nВыберите математическую операцию: \nСложение -- 1\nВычитание -- 2\nУмножение -- 3\nДеление -- 4\nВозведение в степень -- 5\nВычисление квадратного корня -- 6\nВычисление синуса -- 7\nВычисление косинуса -- 8\nВычисление тангенса -- 9\nВычисление факториала -- 10\n'))
     if operation == 1:
-        first_number = int(input('Введите первое число: '))
-        second_number = int(input('Введите второе число: '))
-        result = first_number + second_number
+        try:
+            first_number = int(input('Введите первое число: '))
+            second_number = int(input('Введите второе число: '))
+            result = first_number + second_number
+        except:
+            print('Вы ввели не число!')
+            result = None
     elif operation == 2:
-        first_number = int(input('Введите первое число: '))
-        second_number = int(input('Введите второе число: '))
-        result = first_number - second_number
+        try:
+            first_number = int(input('Введите первое число: '))
+            second_number = int(input('Введите второе число: '))
+            result = first_number - second_number
+        except:
+            print('\nВы ввели не число!')
+            result = None
     elif operation == 3:
-        first_number = int(input('Введите первое число: '))
-        second_number = int(input('Введите второе число: '))
-        result = first_number * second_number
+        try:
+            first_number = int(input('Введите первое число: '))
+            second_number = int(input('Введите второе число: '))
+            result = first_number * second_number
+        except:
+             print('\nВы ввели не число!')
+             result = None
     elif operation == 4:
-        first_number = int(input('Введите первое число: '))
-        second_number = int(input('Введите второе число: '))
-        if second_number == 0:
-            result = None
-            print('Деление на ноль невозможно')
-        else:
-            result = float(first_number / second_number)
+        try:
+            first_number = int(input('Введите первое число: '))
+            second_number = int(input('Введите второе число: '))
+            if second_number == 0:
+                result = None
+                print('Деление на ноль невозможно')
+            else:
+                result = float(first_number / second_number)
+        except:
+             print('\nВы ввели не число!')
+             result = None
     elif operation == 5:
-        number = int(input('Введите число: '))
-        stepen = int(input('Введите степень: '))
-        result = number ** stepen
+        try:
+            first_number = int(input('Введите первое число: '))
+            second_number = int(input('Введите второе число: '))
+            result = first_number ** second_number
+        except:
+             print('\nВы ввели не число!')
+             result = None
     elif operation == 6:
-        number = int(input('Введите число: '))
-        if number >= 0:
-            sqrt = number ** (0.5)
-            result = sqrt
-        else:
+        try:
+            number = int(input('Введите число: '))
+            if number >= 0:
+                sqrt = number ** (0.5)
+                result = sqrt
+            else:
+                result = None
+                print('Корень из отрицательного числа не существует')
+        except:
+            print('\nВы ввели не число!')
             result = None
-            print('Корень из отрицательного числа не существует')
     elif operation == 7:
-        number = int(input('Введите число: '))
-        result = math.sin(number)
+        try:
+            number = int(input('Введите число: '))
+            result = math.sin(number)
+        except:
+            print('\nВы ввели не число!')
+            result = None
     elif operation == 8:
-        number = int(input('Введите число: '))
-        result = math.cos(number)
+        try:
+            number = int(input('Введите число: '))
+            result = math.cos(number)
+        except:
+            print('\nВы ввели не число!')
+            result = None
     elif operation == 9:
-        number = int(input('Введите число: '))
-        result = math.tan(number)
+        try:
+            number = int(input('Введите число: '))
+            result = math.tan(number)
+        except:
+            print('\nВы ввели не число!')
+            result = None
     elif operation == 10:
-        number = int(input('Введите число: '))
-        result = math.factorial(number)
+        try:
+            number = int(input('Введите число: '))
+            if number >= 0:
+                result = math.factorial(number)
+            else:
+                result = None
+                print('Факториал нельзя посчитать из отрицательного числа')
+        except:
+            print('\nВы ввели не число!')
+            result = None
     elif operation == 11:
         print('Калькулятор завершил свою работу!')
         break
